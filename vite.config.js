fadf0f1927c50e85cfeea219ff400798631d02fa
castarokio <elwehch123@gmail.com>
2026-06-17 09:06:05 +0100

    Fix Vercel build asset base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+/* global process */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/travelly/" : "/",
+  base: command === "build" && !process.env.VERCEL ? "/travelly/" : "/",
   plugins: [react()],
 }));
